29cfc4e9575132ebcbfa8ec1f3719137956ed57c
Bisma8090 <bismaarshad8090@gmail.com>
2026-04-21 17:44:28 +0500

    API added

--- ResumeMaker/backend/src/main.ts
+++ ResumeMaker/backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:3000' });
+  app.enableCors({ origin: ['http://localhost:3000', 'https://week08-snowy.vercel.app'] });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.setGlobalPrefix('api');
   await app.listen(3001);
